77fc7541c5f685abcce8d176efed21529ccef07c
Shezan Al-Mahmud <46520348+shezan7@users.noreply.github.com>
2022-01-25 10:54:30 +0600

    Add files via upload

---
+++ AsyncAwait1.js
@@ -0,0 +1,61 @@
+const paymentSuccess = true;
+const marks = 85;
+
+function enroll() {
+    console.log('Course enrollment is in progress...');
+
+    const promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            if(paymentSuccess) {
+                resolve();
+            }
+            else {
+                reject('Payment Failed!!!');
+            }
+        }, 2000);
+    });
+
+    return promise;
+}
+
+function progress() {
+    console.log('Course on progress...');
+
+    const promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            if(marks>=80) {
+                resolve();
+            }
+            else {
+                reject('You could not get enough marks to get the certificate!!!');
+            }
+        }, 3000);
+    });
+
+    return promise;
+}
+
+function getCertificate() {
+    console.log('Preparing your certificate...');
+
+    const promise = new Promise(function(resolve) {
+        setTimeout(function() {
+            resolve('Congratulations!!! You got the certificate!');
+        }, 1000);
+    });
+
+    return promise;
+}
+
+async function course() {
+    try {
+        await enroll();
+        await progress();
+        const message = await getCertificate();
+        console.log(message);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+course();
